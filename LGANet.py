13f3caa6c82b5082eebaca64eae5f40b697778c3
XiaoBai <1490083813@qq.com>
2026-09-03 18:56:59 +0800

    Add files via upload

---
+++ LGANet.py
@@ -0,0 +1,243 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange, repeat
+
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+import timm
+
+
+class ConvBNReLU(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, dilation=1, stride=1, norm_layer=nn.BatchNorm2d, bias=False):
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, bias=bias,
+                      dilation=dilation, stride=stride, padding=((stride - 1) + dilation * (kernel_size - 1)) // 2),
+            norm_layer(out_channels),
+            nn.ReLU6()
+        )
+
+
+class ConvBN(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, dilation=1, stride=1, norm_layer=nn.BatchNorm2d, bias=False):
+        super(ConvBN, self).__init__(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, bias=bias,
+                      dilation=dilation, stride=stride, padding=((stride - 1) + dilation * (kernel_size - 1)) // 2),
+            norm_layer(out_channels)
+        )
+
+
+class Conv(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, dilation=1, stride=1, bias=False):
+        super(Conv, self).__init__(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, bias=bias,
+                      dilation=dilation, stride=stride, padding=((stride - 1) + dilation * (kernel_size - 1)) // 2)
+        )
+
+
+class SeparableConvBNReLU(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, dilation=1,
+                 norm_layer=nn.BatchNorm2d):
+        super(SeparableConvBNReLU, self).__init__(
+            nn.Conv2d(in_channels, in_channels, kernel_size, stride=stride, dilation=dilation,
+                      padding=((stride - 1) + dilation * (kernel_size - 1)) // 2,
+                      groups=in_channels, bias=False),
+            norm_layer(out_channels),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.ReLU6()
+        )
+
+
+class SeparableConvBN(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, dilation=1,
+                 norm_layer=nn.BatchNorm2d):
+        super(SeparableConvBN, self).__init__(
+            nn.Conv2d(in_channels, in_channels, kernel_size, stride=stride, dilation=dilation,
+                      padding=((stride - 1) + dilation * (kernel_size - 1)) // 2,
+                      groups=in_channels, bias=False),
+            norm_layer(out_channels),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+        )
+
+
+class SeparableConv(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, dilation=1):
+        super(SeparableConv, self).__init__(
+            nn.Conv2d(in_channels, in_channels, kernel_size, stride=stride, dilation=dilation,
+                      padding=((stride - 1) + dilation * (kernel_size - 1)) // 2,
+                      groups=in_channels, bias=False),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+        )
+
+
+from geoseg.models.LGANetModel.PCAttention import *
+class DualAttention(nn.Module):
+    def __init__(self, in_channels=128, decode_channels=128, eps=1e-8):
+        super(DualAttention, self).__init__()
+        self.pre_conv = Conv(in_channels, decode_channels, kernel_size=1)
+        self.post_conv = ConvBNReLU(64, decode_channels, kernel_size=3)
+        self.blockUp = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+            ),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+        self.shortcut = ConvBN(decode_channels, decode_channels, kernel_size=1)
+        self.pa = nn.Sequential(
+            nn.Conv2d(decode_channels, decode_channels, kernel_size=3, padding=1, groups=decode_channels),
+            nn.Sigmoid())
+        self.channel_attention = ChannelAttention(decode_channels)
+        self.proj = SeparableConvBN(decode_channels, decode_channels, kernel_size=3)
+        self.act = nn.ReLU6()
+        self.gate = nn.Sequential(
+            nn.Conv2d(decode_channels * 2, decode_channels, 3, padding=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x, res):
+        x = self.blockUp(x)
+        res = self.pre_conv(res)
+        gate_weight = self.gate(torch.cat([x, res], 1))
+        x = gate_weight * x + (1 - gate_weight) * res
+        x = self.post_conv(x)
+        shortcut = self.shortcut(x)
+        pa = self.pa(x) * x
+        avg_out,max_out = self.channel_attention(x)
+        x =  pa + avg_out + max_out
+        x = self.proj(x) + shortcut
+        x = self.act(x)
+
+        return x
+
+
+class FeatureRefinementHead(nn.Module):
+    def __init__(self, in_channels=64, decode_channels=64):
+        super().__init__()
+        self.pre_conv = Conv(in_channels, decode_channels, kernel_size=1)
+        self.post_conv = ConvBNReLU(128, decode_channels, kernel_size=3)
+        self.blockUp = nn.Sequential(
+            # 输入：1x4x4 -> 输出：64x8x8
+            nn.ConvTranspose2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+            ),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+        self.shortcut = ConvBN(decode_channels, decode_channels, kernel_size=1)
+        self.pa = nn.Sequential(
+            nn.Conv2d(decode_channels, decode_channels, kernel_size=3, padding=1, groups=decode_channels),
+            nn.Sigmoid())
+        self.channel_attention = ChannelAttention(decode_channels)
+        self.proj = SeparableConvBN(decode_channels, decode_channels, kernel_size=3)
+        self.act = nn.ReLU6()
+
+    def forward(self, x, res):
+        x = self.blockUp(x)
+        res = self.pre_conv(res)
+        x = torch.cat([x, res], 1)
+        x = self.post_conv(x)
+        shortcut = self.shortcut(x)
+        pa = self.pa(x) * x
+        avg_out, max_out = self.channel_attention(x)
+        x = pa + avg_out + max_out
+        x = self.proj(x) + shortcut
+        x = self.act(x)
+
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self,
+                 encoder_channels=(64, 128, 256, 512),
+                 decode_channels=64,
+                 dropout=0.1,
+                 window_size=8,
+                 num_classes=6):
+        super(Decoder, self).__init__()
+
+        self.pre_conv = ConvBN(encoder_channels[-1], decode_channels, kernel_size=1)
+        self.p3 = DualAttention(encoder_channels[-2], decode_channels)
+        self.p2 = DualAttention(encoder_channels[-3], decode_channels)
+        self.p1 = FeatureRefinementHead(encoder_channels[-4], decode_channels)
+        self.segmentation_head = nn.Sequential(ConvBNReLU(decode_channels, decode_channels),
+                                               nn.Dropout2d(p=dropout, inplace=True),
+                                               Conv(decode_channels, num_classes, kernel_size=1))
+        self.init_weight()
+
+    def forward(self, res1, res2, res3, res4, h, w):
+        if self.training:
+            x = self.pre_conv(res4)
+            x = self.p3(x, res3)
+            x = self.p2(x, res2)
+            x = self.p1(x, res1)
+            x = self.segmentation_head(x)
+            x = F.interpolate(x, size=(h, w), mode='bilinear', align_corners=False)
+
+            return x
+        else:
+            x = self.pre_conv(res4) # 调整通道
+            x = self.p3(x, res3)
+            x = self.p2(x, res2)
+            x = self.p1(x, res1)
+            x = self.segmentation_head(x)
+            x = F.interpolate(x, size=(h, w), mode='bilinear', align_corners=False)
+
+            return x
+
+    def init_weight(self):
+        for m in self.children():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, a=1)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+from geoseg.models.LGANetModel.LKDGF import *
+class LGANet(nn.Module):
+    def __init__(self,
+                 decode_channels=64,
+                 dropout=0.1,
+                 backbone_name='swsl_resnet18',
+                 pretrained=True,
+                 window_size=8,
+                 num_classes=6
+                 ):
+        super().__init__()
+
+        self.backbone = timm.create_model(backbone_name, features_only=True, output_stride=32,
+                                          out_indices=(1, 2, 3, 4), pretrained=pretrained)
+        encoder_channels = self.backbone.feature_info.channels()
+
+        self.decoder = Decoder(encoder_channels, decode_channels, dropout, window_size, num_classes)
+
+
+        self.downsample_2x = nn.Sequential(
+            nn.Conv2d(3, 128, 3, stride=2, padding=1), 
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            ConvBNReLU(128, decode_channels, kernel_size=3)
+        )
+
+        self.lkdf = LKD_FusionFeature(num_features=4)
+
+    def forward(self, x):
+
+        h, w = x.size()[-2:]
+        res1, res2, res3, res4 = self.backbone(x)
+
+        lkd_x = self.downsample_2x(x)
+        res1, res2, res3, res4 = self.lkdf(lkd_x, res1, res2, res3, res4)
+
+        if self.training:
+            x = self.decoder(res1, res2, res3, res4, h, w)
+            return x
+        else:
+            x = self.decoder(res1, res2, res3, res4, h, w)
+            return x
---
+++ PCAttention.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, reduction_ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels // reduction_ratio, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels // reduction_ratio, in_channels, 1, bias=False)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+
+        avg_out = self.sigmoid(self.fc(self.avg_pool(x))) * x
+        max_out = self.sigmoid(self.fc(self.max_pool(x))) * x
+        return avg_out,max_out
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        self.conv = nn.Conv2d(2, 1, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv(x)
+        return self.sigmoid(x)
